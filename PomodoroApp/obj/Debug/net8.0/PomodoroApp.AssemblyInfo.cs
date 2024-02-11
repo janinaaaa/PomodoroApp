61edbb91fe811d2efcc32f4191372c239f3953a8
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-02-11 19:57:12 +0100

    improved timer management and added buttons for pause

--- PomodoroApp/obj/Debug/net8.0/PomodoroApp.AssemblyInfo.cs
+++ PomodoroApp/obj/Debug/net8.0/PomodoroApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PomodoroApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6cb492e96ebfaaf2d68e0161efee2d354256c7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3a069387c6947d879fcb647be50ab5a540aea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PomodoroApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PomodoroApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
